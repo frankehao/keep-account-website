c59eec9b96913286d7ec98285f62afdd8b5066e9
hao <2524129018@qq.com>
2022-06-15 09:53:32 +0800

    deploy

---
+++ precache-manifest.c92a9934da6c88f699075c9410c479cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28dfba2b20b9c61bfd032a4c2a203713",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e77c82c66dd4c23fee8",
+    "url": "/static/css/main.899c3c20.chunk.css"
+  },
+  {
+    "revision": "4da86b069fb102d001d9",
+    "url": "/static/js/2.f14af5f1.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/static/js/2.f14af5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e77c82c66dd4c23fee8",
+    "url": "/static/js/main.eb2ca393.chunk.js"
+  },
+  {
+    "revision": "a730e0e44c18fd6ac02e",
+    "url": "/static/js/runtime-main.ee3bab4f.js"
+  }
+]);
